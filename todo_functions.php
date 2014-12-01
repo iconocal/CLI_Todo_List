9aa252a1dc2b4f9a1659a688f31153b7fdcc3052
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-01 14:43:20 -0600

    new todo with functions, a do-over

---
+++ todo_functions.php
@@ -0,0 +1,59 @@
+<?php
+
+ // Create array to hold list of todo items
+ $items = array();
+
+ // List array items formatted for CLI
+ function listItems($list) {
+    $todoItem = '';
+    foreach ($list as $key => $item) {
+        $todoItem .= "$key $item\n";
+        $key++;
+    }
+    return "$todoItem\n";
+}
+
+// Defined Function: Get Input
+// Get STDIN, strip whitespace and newlines,
+// and convert to uppercase if $upper is true
+
+ function getInput($upper = false)
+ {
+     $input = strtoupper(trim(fgets(STDIN)));
+     return $input;
+ }
+
+ // The loop!
+ do {
+     // Echo the list produced by the function
+     echo listItems($items);
+
+     // Show the menu options
+     echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+     // Get the input from user
+     // Use trim() to remove whitespace and newlines
+     $input = getInput(true);
+
+     // Check for actionable input
+     if ($input == 'N') {
+         // Ask for entry
+         echo 'Enter item: ';
+         // Add entry to list array
+         $items[] = getInput();
+     } elseif ($input == 'R') {
+         // Remove which item?
+         echo 'Enter item number to remove: ';
+         // Get array key
+         $key = getInput();
+         // Remove from array
+         unset($items[$key]);
+     }
+ // Exit when input is (Q)uit
+ } while ($input != 'Q');
+
+ // Say Goodbye!
+ echo "Goodbye!\n";
+
+ // Exit with 0 errors
+ exit(0);
